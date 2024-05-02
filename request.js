ed5107df24e03946bebba422fbc0e4e3cd4272eb
lyyxyq <1543177232@qq.com>
2024-05-02 15:17:58 +0800

    🎉 init: 初始化

---
+++ request.js
@@ -0,0 +1,52 @@
+/* 封装axios用于发送请求 */
+// import store from '@/store'
+import axios from 'axios'
+// import router from '../router'
+
+// 创建一个新的axios实例
+const userStore = useUserStore()
+const request = axios.create({
+  baseURL: 'https://119.3.15.195:8081/',
+  timeout: 2000
+})
+
+// 添加请求拦截器
+request.interceptors.request.use(
+  function (config) {
+    // 在发送请求之前做些什么
+    const token = localStorage.getItem('token')
+    console.log(token)
+    if (token) {
+      // console.log(token)
+      config.headers.Authorization = 'Bearer ' + token
+    }
+    return config
+  },
+  function (error) {
+    // 对请求错误做些什么
+
+    return Promise.reject(error)
+  }
+)
+
+// // 添加响应拦截器
+// request.interceptors.response.use(
+//   function (response) {
+//     // 对响应数据做点什么
+//     const res = response.data
+//     if (res.status !== 200) {
+//       return Promise.reject(res.message)
+//     }
+//     return res
+//   },
+//   function (error) {
+//     // 对响应错误做点什么
+//     if (error.response.status === 403) {
+//       router.push('login')
+//     }
+//     alert('数据相应失败')
+//     return Promise.reject(error)
+//   }
+// )
+
+export default request
